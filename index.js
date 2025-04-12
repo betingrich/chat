4c64bad719f0305c0eb29004744605296e12b0f2
marisel <140407389+betingrich@users.noreply.github.com>
2025-04-13 02:14:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,72 +1,78 @@
-// Setup basic express server
 const express = require('express');
 const app = express();
 const path = require('path');
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+const mongoose = require('mongoose');
 const port = process.env.PORT || 3000;
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://ellyongiro8:<db_password>@cluster0.tyxcmm9.mongodb.net/chatapp?retryWrites=true&w=majority&appName=Cluster0', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+// Define schema and model
+const messageSchema = new mongoose.Schema({
+  username: String,
+  message: String,
+  timestamp: { type: Date, default: Date.now }
+});
+const Message = mongoose.model('Message', messageSchema);
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 server.listen(port, () => {
   console.log('Server listening at port %d', port);
 });
-
-// Routing
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Chatroom
 
 let numUsers = 0;
 
 io.on('connection', (socket) => {
   let addedUser = false;
 
-  // when the client emits 'new message', this listens and executes
-  socket.on('new message', (data) => {
-    // we tell the client to execute 'new message'
-    socket.broadcast.emit('new message', {
-      username: socket.username,
-      message: data
-    });
-  });
-
-  // when the client emits 'add user', this listens and executes
-  socket.on('add user', (username) => {
+  socket.on('add user', async (username) => {
     if (addedUser) return;
-
-    // we store the username in the socket session for this client
     socket.username = username;
     ++numUsers;
     addedUser = true;
-    socket.emit('login', {
-      numUsers: numUsers
-    });
-    // echo globally (all clients) that a person has connected
+
+    const messages = await Message.find().sort({ timestamp: 1 }).limit(50);
+    socket.emit('chat history', messages);
+
+    socket.emit('login', { numUsers });
     socket.broadcast.emit('user joined', {
       username: socket.username,
       numUsers: numUsers
     });
   });
 
-  // when the client emits 'typing', we broadcast it to others
+  socket.on('new message', async (data) => {
+    const newMsg = new Message({
+      username: socket.username,
+      message: data
+    });
+    await newMsg.save();
+
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
+  });
+
   socket.on('typing', () => {
-    socket.broadcast.emit('typing', {
-      username: socket.username
-    });
+    socket.broadcast.emit('typing', { username: socket.username });
   });
 
-  // when the client emits 'stop typing', we broadcast it to others
   socket.on('stop typing', () => {
-    socket.broadcast.emit('stop typing', {
-      username: socket.username
-    });
+    socket.broadcast.emit('stop typing', { username: socket.username });
   });
 
-  // when the user disconnects.. perform this
   socket.on('disconnect', () => {
     if (addedUser) {
       --numUsers;
-
-      // echo globally that this client has left
       socket.broadcast.emit('user left', {
         username: socket.username,
         numUsers: numUsers
