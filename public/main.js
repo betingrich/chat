06f21d56964b148ddb19cfe5bba1a8e483fb5442
marisel <140407389+betingrich@users.noreply.github.com>
2025-04-13 02:44:52 +0300

    Update main.js

--- public/main.js
+++ public/main.js
@@ -1,58 +1,105 @@
 $(function() {
-  const socket = io();
+  const FADE_TIME = 150;
+  const TYPING_TIMER_LENGTH = 400;
+  const COLORS = [
+    '#e21400', '#91580f', '#f8a700', '#f78b00',
+    '#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
+    '#3b88eb', '#3824aa', '#a700ff', '#d300e7'
+  ];
 
-  const $loginPage = $('.login.page');
-  const $chatPage = $('.chat.page');
+  // DOM elements
+  const $window = $(window);
   const $usernameInput = $('.usernameInput');
   const $messages = $('.messages');
   const $inputMessage = $('.inputMessage');
-  const $sendButton = $('.sendButton');
+  const $loginPage = $('.login.page');
+  const $chatPage = $('.chat.page');
 
-  let username;
+  const socket = io();
+
+  // User state
+  let username = '';
+  let connected = false;
+  let typing = false;
+  let lastTypingTime;
+  let $currentInput = $usernameInput.focus();
+
+  // Helper functions
+  const addParticipantsMessage = (data) => {
+    const message = data.numUsers === 1 
+      ? "there's 1 participant" 
+      : `there are ${data.numUsers} participants`;
+    log(message);
+  };
 
   const setUsername = () => {
-    username = $usernameInput.val().trim();
+    username = cleanInput($usernameInput.val().trim());
+
     if (username) {
-      $loginPage.hide();
+      $loginPage.fadeOut();
       $chatPage.show();
+      $loginPage.off('click');
+      $currentInput = $inputMessage.focus();
       socket.emit('add user', username);
     }
   };
 
   const sendMessage = () => {
-    const message = $inputMessage.val().trim();
-    if (message) {
+    const message = cleanInput($inputMessage.val());
+    if (message && connected) {
       $inputMessage.val('');
       addChatMessage({ username, message });
       socket.emit('new message', message);
+      socket.emit('stop typing');
+      typing = false;
     }
   };
 
-  const addChatMessage = (data) => {
-    const $message = $('<li class="message"/>')
-      .append($('<span class="username"/>').text(data.username + ": "))
-      .append($('<span class="messageBody"/>').text(data.message));
-    $messages.append($message);
-    $messages.scrollTop($messages[0].scrollHeight);
+  // Message display functions
+  const log = (message, options) => {
+    const $el = $('<li>').addClass('log').text(message);
+    addMessageElement($el, options);
   };
 
-  $usernameInput.keypress(event => {
-    if (event.which === 13) {
-      setUsername();
+  const addChatMessage = (data, options = {}) => {
+    const $typingMessages = getTypingMessages(data);
+    if ($typingMessages.length !== 0) {
+      options.fade = false;
+      $typingMessages.remove();
     }
+
+    const $usernameDiv = $('<span class="username"/>')
+      .text(data.username)
+      .css('color', getUsernameColor(data.username));
+    const $messageBodyDiv = $('<span class="messageBody">').text(data.message);
+
+    const $messageDiv = $('<li class="message"/>')
+      .data('username', data.username)
+      .append($usernameDiv, $messageBodyDiv);
+
+    addMessageElement($messageDiv, options);
+  };
+
+  // Socket event handlers
+  socket.on('login', (data) => {
+    connected = true;
+    log('Welcome to the chatroom');
+    addParticipantsMessage(data);
   });
 
-  $inputMessage.keypress(event => {
-    if (event.which === 13) {
-      sendMessage();
-    }
-  });
-
-  $sendButton.click(() => {
-    sendMessage();
-  });
-
-  socket.on('new message', data => {
+  socket.on('new message', (data) => {
     addChatMessage(data);
   });
+
+  socket.on('load messages', (messages) => {
+    messages.forEach(msg => {
+      addChatMessage({
+        username: msg.username,
+        message: msg.message
+      }, { fade: false });
+    });
+  });
+
+  // Rest of your existing event handlers...
+  // (Keep all your existing typing, user joined/left handlers)
 });
